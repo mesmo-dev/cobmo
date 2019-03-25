bc33cfa615f79a687f1dfbe45dc6d451f61409e4
Sebastian Troitzsch <s@troitzs.ch>
2019-03-25 11:09:04 +0800

    Template: Minor update

--- template.py
+++ template.py
@@ -1,5 +1,4 @@
-"""
-Script template
+"""Script template
 
 - This script is a template
 - It prints "Hello world!"
@@ -9,21 +8,19 @@ import os # Replace this with actual imports.
 
 
 def template_function():
-    """
-    This is where the action happens if it is more than a few lines in ``main``.
+    """This is where the action happens if it is more than a few lines in ``main``.
+
+    - Extended description.
     """
 
     print('Hello world!')
 
 
 def main():
-    """
-    This is the main entry point to your script.
-    """
+    """This is the main entry point to your script."""
 
     template_function()
 
 
 if __name__ == '__main__':
     main()
-
