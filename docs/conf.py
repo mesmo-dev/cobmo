c2a6c1738ab01bfa156b5556108fba7e8486b385
Sebastian Troitzsch <s@troitzs.ch>
2019-07-10 18:23:07 +0800

    Docs: Add settings for Readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -35,19 +35,20 @@ author = 'TUMCREATE'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
     'sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
+source_suffix = '.md'
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
