8f7b39e337324d500b6f408429dfee1304684392
Tommaso Miori <tommaso.miori@gmail.com>
2019-08-28 18:48:10 +0800

    main_bes: cleaning and adjusting

--- cobmo/main_bes.py
+++ cobmo/main_bes.py
@@ -107,42 +107,6 @@ def example():
         control_timeseries=control_timeseries_simulation
     )
 
-    # Outputs for debugging
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.state_matrix=")
-    # print(building.state_matrix)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.control_matrix=")
-    # print(building.control_matrix)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.disturbance_matrix=")
-    # print(building.disturbance_matrix)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.state_output_matrix=")
-    # print(building.state_output_matrix)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.control_output_matrix=")
-    # print(building.control_output_matrix)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.disturbance_output_matrix=")
-    # print(building.disturbance_output_matrix)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("control_timeseries_simulation=")
-    # print(control_timeseries_simulation)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("building.disturbance_timeseries=")
-    # print(building.disturbance_timeseries)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("state_timeseries_simulation=")
-    # print(state_timeseries_simulation)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("output_timeseries_simulation=")
-    # print(output_timeseries_simulation)
-    # print("-----------------------------------------------------------------------------------------------------------")
-
-    # file_output_text = open("my_file_out.txt", "w")
-    # file_output_text.write(building.state_matrix)
-
     # Run controller
     controller = cobmo.controller_bes.Controller_bes(
         conn=conn,
@@ -160,8 +124,7 @@ def example():
 
     # Printing and Plotting
 
-    print_on_csv = 1  # set to 1 to print results in csv files (not tracked by the git)
-    plotting = 1  # set 1 for plotting (to save the plot set "save_plot_on_off" to on
+    print_on_csv = 0  # set to 1 to print results in csv files (not tracked by the git)
 
     # if storage_size is not None:
     if 'storage' in building.building_scenarios['building_storage_type'][0]:
@@ -170,64 +133,34 @@ def example():
         print('\n>> Storage size = %.2f kWh' % storage_size_kwh)
         print('\n>> Total opex + capex (storage)= {}'.format(format(optimum_obj, '.2f')))
 
-        if plotting == 1:
-            # Calculating the savings and the payback time
-            costs_without_storage = 3.834195403e+02  # [SGD/day], 14 levels
-            savings_day = (costs_without_storage - optimum_obj)
-            storage_investment_per_unit = building.building_scenarios['storage_investment_sgd_per_unit'][0]
-            (payback, payback_df) = cobmo.utils.discounted_payback_time(
-                building,
-                storage_size_kwh,
-                storage_investment_per_unit,
-                12.0,  # storage lifetime as input
-                savings_day,
-                save_plot_on_off='off'  # "on" to save the plot as .svg (not tracked by the git)
-            )
+        # Calculating the savings and the payback time
+        costs_without_storage = 3.834195403e+02  # [SGD/day], 14 levels
+        savings_day = (costs_without_storage - optimum_obj)
+        storage_investment_per_unit = building.building_scenarios['storage_investment_sgd_per_unit'][0]
+        (payback, payback_df) = cobmo.utils.discounted_payback_time(
+            building,
+            storage_size_kwh,
+            storage_investment_per_unit,
+            12.0,  # storage lifetime as input
+            savings_day,
+            save_plot_on_off='off',  # "on" to save the plot as .svg (not tracked by the git)
+            plotting_on_off=1  # set 1 for plotting
+        )
 
-            print('\n>> Storage type = %s'
-                  '  |  Optimal storage size = %.2f'
-                  '  | savings year 〜= %.2f'
-                  '  | Discounted payback = %i\n'
-                  % (
-                    building.building_scenarios['building_storage_type'][0],
-                    storage_size,
-                    savings_day * 260.0,
-                    payback
-                  )
-                  )
+        print('\n>> Storage type = %s'
+              '  |  Optimal storage size = %.2f'
+              '  | savings year 〜= %.2f'
+              '  | Discounted payback = %i\n'
+              % (
+                building.building_scenarios['building_storage_type'][0],
+                storage_size,
+                savings_day * 260.0,
+                payback
+              )
+              )
     else:
         print('\n----------------------------------------------')
         print('\n>> Total opex (baseline)= {}\n'.format(format(optimum_obj, '.2f')))
-
-    # # Outputs for debugging
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("control_timeseries_controller=")
-    # print(control_timeseries_controller)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("state_timeseries_controller=")
-    # print(state_timeseries_controller)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("output_timeseries_controller=")
-    # print(output_timeseries_controller)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    #
-    # # Run error calculation function
-    # (
-    #     error_summary,
-    #     error_timeseries
-    # ) = cobmo.utils.calculate_error(
-    #     output_timeseries_simulation.loc[:, output_timeseries_controller.columns.str.contains('temperature')],
-    #     output_timeseries_controller.loc[:, output_timeseries_controller.columns.str.contains('temperature')]
-    # )  # Note: These are exemplary inputs.
-    #
-    # # Outputs for debugging
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("error_timeseries=")
-    # print(error_timeseries)
-    # print("-----------------------------------------------------------------------------------------------------------")
-    # print("error_summary=")
-    # print(error_summary)
-    # print("-----------------------------------------------------------------------------------------------------------")
 
     if print_on_csv == 1:
         if ((building.building_scenarios['building_storage_type'][0] == 'sensible_thermal_storage_default')
