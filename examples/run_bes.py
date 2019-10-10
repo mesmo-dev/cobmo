0abf6a0dbcfc52466ad007052401cf62ff4478a3
Sebastian Troitzsch <s@troitzs.ch>
2019-10-10 14:36:59 +0800

    run_bes: Save CSV to new results folder + more cleanup

--- examples/run_bes.py
+++ examples/run_bes.py
@@ -1,19 +1,11 @@
-"""Use this script for single simulations of battery storage.
+"""Run script for single simulation / optimization of battery storage."""
 
-Please note:
-- The solution depends on the lifetime assigned to the storage in `building_storage_types.csv`.
-- Changing the lifetime changes the results.
-
-TODO: Check if the following points are still valid:
-- The lifetime has to be usually very high (100 or 150) to have storage installed.
-- The results from this script are not totally coherent from an economic analysis point of view.
-"""
-
-import datetime as dt
 import numpy as np
+import os
 import pandas as pd
 
 import cobmo.building
+import cobmo.config
 import cobmo.controller_bes
 import cobmo.database_interface
 import cobmo.utils
@@ -21,7 +13,20 @@ import cobmo.utils
 
 # Settings.
 scenario_name = 'scenario_default'
-pricing_method = 'wholesale_market'  # Options: 'wholesale_market' or 'retailer_peak_offpeak'.
+pricing_method = 'wholesale_market'  # Choices: 'wholesale_market', 'retailer_peak_offpeak'.
+building_storage_type = 'battery_storage_default'  # Choices: 'battery_storage_default', ''.
+save_csv = 1
+plotting = 0
+save_plot = 0
+
+# Set results path and create the directory.
+# TODO: Throw error when thermal instead of battery storage.
+if save_csv == 1:
+    if building_storage_type == 'battery_storage_default':
+        results_path = os.path.join(cobmo.config.results_path, 'run_bes__with_storage_' + cobmo.config.timestamp)
+    else:
+        results_path = os.path.join(cobmo.config.results_path, 'run_bes__without_storage_' + cobmo.config.timestamp)
+    os.mkdir(results_path)
 
 # Obtain a connection to the database.
 conn = cobmo.database_interface.connect_database()
@@ -44,7 +49,7 @@ buildings = pd.read_sql(
 
 # Modify `building_storage_type` for the current scenario in the database.
 building_name = building_scenarios.at[scenario_name, 'building_name']
-buildings.at[building_name, 'building_storage_type'] = 'battery_storage_default'
+buildings.at[building_name, 'building_storage_type'] = building_storage_type
 buildings.to_sql(
     'buildings',
     con=conn,
@@ -93,20 +98,10 @@ controller = cobmo.controller_bes.Controller_bes(
     optimum_obj
 ) = controller.solve()
 
-# Printing and plotting settings.
-print_on_csv = 0
-plotting = 1
-save_plot = 0
-
-# If storage_size is not None:
-if 'storage' in building.building_scenarios['building_storage_type'][0]:
-    storage_size_kwh = storage_size * 3.6e-3 * 1.0e-3
-    print('\n----------------------------------------------')
-    print('\n>> Storage size = %.2f kWh' % storage_size_kwh)
-    print('\n>> Total opex + capex (storage)= {}'.format(format(optimum_obj, '.2f')))
-
-    # Calculating the savings and the payback time
-    costs_without_storage = 3.834195403e+02  # [SGD/day], 14 levels
+if building_storage_type == 'battery_storage_default':
+    # Calculate savings and payback time.
+    storage_size_kwh = storage_size * 3.6e-3 * 1.0e-3  # Ws to kWh
+    costs_without_storage = 3.834195403e+02  # [SGD/day], 14 levels, for CREATE Tower. # TODO: should not be hardcoded.
     savings_day = (costs_without_storage - optimum_obj)
     (payback, payback_df) = cobmo.utils.discounted_payback_time(
         building,
@@ -117,69 +112,34 @@ if 'storage' in building.building_scenarios['building_storage_type'][0]:
         storage='battery'
     )
 
-    print('\n>> Storage type = %s'
-          '  |  Optimal storage size = %.2f'
-          '  | savings year 〜= %.2f'
-          '  | Discounted payback = %i\n'
-          % (
-            building.building_scenarios['building_storage_type'][0],
-            storage_size,
-            savings_day * 260.0,
-            payback
-          )
-          )
+    # Print results.
+    print("Storage size = {} kWh".format(round(storage_size_kwh, 2)))
+    print("Total OPEX + CAPEX with storage = {}".format(round(optimum_obj, 2)))
+    print("Storage type = {}".format(building.building_scenarios['building_storage_type'][0]))
+    print("Optimal storage size = {}".format(round(storage_size, 2)))
+    print("Savings per year ~= {}".format(round(savings_day * 260.0, 2)))
+    print("Discounted payback = {}".format(round(payback)))
 else:
-    print('\n----------------------------------------------')
-    print('\n>> Total opex (baseline)= {}\n'.format(format(optimum_obj, '.2f')))
+    # Print results.
+    print("Total OPEX without storage = {}".format(round(optimum_obj, 2)))
 
-if print_on_csv == 1:
-    if ((building.building_scenarios['building_storage_type'][0] == 'sensible_thermal_storage_default')
-            or (building.building_scenarios['building_storage_type'][0] == 'latent_thermal_storage_default')
-            or (building.building_scenarios['building_storage_type'][0] == 'battery_storage_default')):
+# Save results to CSV.
+if save_csv == 1:
+    building.state_matrix.to_csv(os.path.join(results_path, 'building_state_matrix.csv'))
+    building.control_matrix.to_csv(os.path.join(results_path, 'building_control_matrix.csv'))
+    building.disturbance_matrix.to_csv(os.path.join(results_path, 'building_disturbance_matrix.csv'))
+    building.state_output_matrix.to_csv(os.path.join(results_path, 'building_state_output_matrix.csv'))
+    building.control_output_matrix.to_csv(os.path.join(results_path, 'building_control_output_matrix.csv'))
+    building.disturbance_output_matrix.to_csv(os.path.join(results_path, 'building_disturbance_output_matrix.csv'))
 
-        building.state_matrix.to_csv('delete_me_storage/bes/state_matrix_BES.csv')
-        building.control_matrix.to_csv('delete_me_storage/bes/control_matrix_BES.csv')
-        building.disturbance_matrix.to_csv('delete_me_storage/bes/disturbance_matrix_BES.csv')
+    control_timeseries_simulation.to_csv(os.path.join(results_path, 'control_timeseries_simulation.csv'))
+    state_timeseries_simulation.to_csv(os.path.join(results_path, 'state_timeseries_simulation.csv'))
+    output_timeseries_simulation.to_csv(os.path.join(results_path, 'output_timeseries_simulation.csv'))
 
-        building.state_output_matrix.to_csv('delete_me_storage/bes/state_output_matrix_BES.csv')
-        building.control_output_matrix.to_csv('delete_me_storage/bes/control_output_matrix_BES.csv')
-        building.disturbance_output_matrix.to_csv('delete_me_storage/bes/disturbance_output_matrix_BES.csv')
+    control_timeseries_controller.to_csv(os.path.join(results_path, 'control_timeseries_controller.csv'))
+    state_timeseries_controller.to_csv(os.path.join(results_path, 'state_timeseries_controller.csv'))
+    output_timeseries_controller.to_csv(os.path.join(results_path, 'output_timeseries_controller.csv'))
 
-        # np.savetxt(r'my_file_output_state_matrix.txt', building.state_matrix.values) # , fmt='%d'
-        state_timeseries_simulation.to_csv('delete_me_storage/bes/state_timeseries_simulation_BES.csv')
-
-        state_timeseries_controller.to_csv('delete_me_storage/bes/state_timeseries_controller_BES.csv')
-        date_main = dt.datetime.now()
-        filename_out_controller = (
-                'output_timeseries_controller_BES' + '_{:04d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}'.format(
-                    date_main.year, date_main.month, date_main.day, date_main.hour, date_main.minute,
-                    date_main.second)
-                + '.csv'
-        )
-        output_timeseries_controller.to_csv('delete_me_storage/bes/' + filename_out_controller)
-
-        control_timeseries_controller.to_csv('delete_me_storage/bes/control_timeseries_controller_BES.csv')
-
-    else:
-        building.state_matrix.to_csv('delete_me/state_matrix.csv')
-        building.control_matrix.to_csv('delete_me/control_matrix.csv')
-        building.disturbance_matrix.to_csv('delete_me/disturbance_matrix.csv')
-
-        building.state_output_matrix.to_csv('delete_me/state_output_matrix.csv')
-        building.control_output_matrix.to_csv('delete_me/control_output_matrix.csv')
-        building.disturbance_output_matrix.to_csv('delete_me/disturbance_output_matrix.csv')
-
-        # np.savetxt(r'my_file_output_state_matrix.txt', building.state_matrix.values) # , fmt='%d'
-        state_timeseries_simulation.to_csv('delete_me/state_timeseries_simulation.csv')
-
-        state_timeseries_controller.to_csv('delete_me/state_timeseries_controller.csv')
-
-        date_main = dt.datetime.now()
-        filename_out_controller = (
-                'output_timeseries_controller' + '_{:04d}-{:02d}-{:02d}_{:02d}-{:02d}-{:02d}'.format(
-                    date_main.year, date_main.month, date_main.day, date_main.hour, date_main.minute,
-                    date_main.second)
-                + '.csv'
-        )
-        output_timeseries_controller.to_csv('delete_me/' + filename_out_controller)
-        control_timeseries_controller.to_csv('delete_me/control_timeseries_controller.csv')
+# Print results path.
+if save_csv == 1:
+    print("Results are stored in: " + results_path)
