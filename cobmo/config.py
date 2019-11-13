bde7f7f5f18052ef05aedf40ee3f0cf7c1192555
Sebastian Troitzsch <s@troitzs.ch>
2019-11-13 13:54:08 +0800

    config: Silence optimization solver output by default

--- cobmo/config.py
+++ cobmo/config.py
@@ -14,7 +14,7 @@ timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 # Optimization solver settings.
 solver_name = 'gurobi'  # Must be valid input string for Pyomo's `SolverFactory`.
-solver_output = True  # If True, activate verbose solver output.
+solver_output = False  # If True, activate verbose solver output.
 
 # Logger settings.
 logging_level = logging.DEBUG
