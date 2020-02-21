7d29ac2b1f91549d0aeca66205ada25af3f1abb7
Sebastian Troitzsch <s@troitzs.ch>
2020-02-21 19:10:47 +0800

    test_building_model: Use parameterized tests for all scenarios

--- tests/test_building_model.py
+++ tests/test_building_model.py
@@ -1,6 +1,7 @@
-"""Tests for `cobmo.building`."""
+"""Tests for building models."""
 
 import pandas as pd
+from parameterized import parameterized
 import time
 import unittest
 
@@ -9,6 +10,19 @@ import cobmo.config
 import cobmo.database_interface
 
 logger = cobmo.config.get_logger(__name__)
+
+# Obtain database connection.
+database_connection = cobmo.database_interface.connect_database()
+
+# Obtain scenario names.
+scenario_names = (
+    pd.read_sql(
+        """
+        SELECT scenario_name FROM building_scenarios 
+        """,
+        database_connection
+    )['scenario_name'].tolist()
+)
 
 
 class TestBuilding(unittest.TestCase):
@@ -23,26 +37,14 @@ class TestBuilding(unittest.TestCase):
         time_duration = time.time() - time_start
         logger.info(f"Test BuildingModel for scenario '{scenario_name}': Completed in {time_duration:.6f} seconds.")
 
-    def test_building_model_all_scenarios(self):
-        # Setup.
-        database_connection = cobmo.database_interface.connect_database()
-        building_scenarios = (
-            pd.read_sql(
-                """
-                SELECT scenario_name FROM building_scenarios 
-                """,
-                database_connection
-            )
-        )
-
-        # Iterate through all scenarios.
-        for scenario_name in building_scenarios['scenario_name'].tolist():
-
-            # Get result.
-            time_start = time.time()
-            cobmo.building_model.BuildingModel(scenario_name, database_connection)
-            time_duration = time.time() - time_start
-            logger.info(f"Test BuildingModel for scenario '{scenario_name}': Completed in {time_duration:.6f} seconds.")
+    @parameterized.expand(
+        [(scenario_name,) for scenario_name in scenario_names])
+    def test_building_model_any_scenario(self, scenario_name):
+        # Get result.
+        time_start = time.time()
+        cobmo.building_model.BuildingModel(scenario_name, database_connection)
+        time_duration = time.time() - time_start
+        logger.info(f"Test BuildingModel for scenario '{scenario_name}': Completed in {time_duration:.6f} seconds.")
 
 
 if __name__ == '__main__':
