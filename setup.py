e06cbeda8109225581df193845800b34d51f4f2a
Sebastian Troitzsch <s@troitzs.ch>
2020-05-04 22:33:48 +0800

    docs & setup & github: Change docs config / workflow to not require package installation

--- setup.py
+++ setup.py
@@ -7,8 +7,10 @@ setuptools.setup(
     version='0.3.0',
     py_modules=setuptools.find_packages(),
     install_requires=[
+        # Please note: Dependencies must also be added in `docs/conf.py` to `autodoc_mock_imports`.
         'CoolProp==6.2.1',
         'hvplot',
+        'matplotlib',
         'multimethod',
         'numpy',
         'pandas',
