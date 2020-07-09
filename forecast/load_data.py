915bbbdb2fc39d456a2b7a7033ea4155204c0cb7
Christian Utama <christianutama@live.com>
2020-07-09 14:32:24 +0800

    forecast: Minor fix

--- forecast/load_data.py
+++ forecast/load_data.py
@@ -4,7 +4,7 @@ import cobmo.config
 
 
 def main():
-    forecast_path = os.path.join(cobmo.config.cobmo_path, 'forecast')
+    forecast_path = os.path.join(cobmo.config.base_path, 'forecast')
     data_path = os.path.join(forecast_path, 'data')
     file_list = os.listdir(data_path)
     df = pd.DataFrame()
