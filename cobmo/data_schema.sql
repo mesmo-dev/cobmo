222b3ba2a77e1576dbb3cb86e175a80e6cb066bf
Sebastian Troitzsch <s@troitzs.ch>
2021-02-02 16:15:14 +0800

    data_schema: Add SQL primary key constraints (for catching CSV definition errors early.)

--- cobmo/data_schema.sql
+++ cobmo/data_schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS buildings (
     weather_type TEXT,
     plant_cooling_type TEXT,
     plant_heating_type TEXT,
-    storage_type TEXT
+    storage_type TEXT,
+    PRIMARY KEY(building_name)
 );
 CREATE TABLE IF NOT EXISTS constraint_schedules (
     constraint_type TEXT,
@@ -15,12 +16,14 @@ CREATE TABLE IF NOT EXISTS constraint_schedules (
     minimum_fresh_air_flow_occupants TEXT,
     maximum_co2_concentration TEXT,
     minimum_relative_humidity TEXT,
-    maximum_relative_humidity TEXT
+    maximum_relative_humidity TEXT,
+    PRIMARY KEY(constraint_type,time_period)
 );
 CREATE TABLE IF NOT EXISTS electricity_price_timeseries (
     price_type TEXT,
     time TEXT,
-    price REAL
+    price REAL,
+    PRIMARY KEY(price_type,time)
 );
 CREATE TABLE IF NOT EXISTS electricity_price_range (
     time_period TEXT,
@@ -35,12 +38,14 @@ CREATE TABLE IF NOT EXISTS hvac_ahu_types (
     ahu_fan_efficiency TEXT,
     ahu_cooling_efficiency TEXT,
     ahu_heating_efficiency TEXT,
-    ahu_return_air_heat_recovery_efficiency TEXT
+    ahu_return_air_heat_recovery_efficiency TEXT,
+    PRIMARY KEY(hvac_ahu_type)
 );
 CREATE TABLE IF NOT EXISTS hvac_generic_types (
     hvac_generic_type TEXT,
     generic_heating_efficiency TEXT,
-    generic_cooling_efficiency TEXT
+    generic_cooling_efficiency TEXT,
+    PRIMARY KEY(hvac_generic_type)
 );
 CREATE TABLE IF NOT EXISTS hvac_radiator_types (
     hvac_radiator_type TEXT,
@@ -55,7 +60,8 @@ CREATE TABLE IF NOT EXISTS hvac_radiator_types (
     radiator_hull_conductivity TEXT,
     radiator_hull_heat_capacity TEXT,
     radiator_fin_effectiveness TEXT,
-    radiator_heating_efficiency TEXT
+    radiator_heating_efficiency TEXT,
+    PRIMARY KEY(hvac_radiator_type)
 );
 CREATE TABLE IF NOT EXISTS hvac_tu_types (
     hvac_tu_type TEXT,
@@ -63,11 +69,13 @@ CREATE TABLE IF NOT EXISTS hvac_tu_types (
     tu_supply_air_temperature_setpoint TEXT,
     tu_fan_efficiency TEXT,
     tu_cooling_efficiency TEXT,
-    tu_heating_efficiency TEXT
+    tu_heating_efficiency TEXT,
+    PRIMARY KEY(hvac_tu_type)
 );
 CREATE TABLE IF NOT EXISTS hvac_vent_types (
     hvac_vent_type TEXT,
-    vent_fan_efficiency TEXT
+    vent_fan_efficiency TEXT,
+    PRIMARY KEY(hvac_vent_type)
 );
 CREATE TABLE IF NOT EXISTS initial_state_types (
     initial_state_type TEXT,
@@ -75,21 +83,24 @@ CREATE TABLE IF NOT EXISTS initial_state_types (
     initial_surface_temperature TEXT,
     initial_co2_concentration TEXT,
     initial_absolute_humidity TEXT,
-    initial_storage_state_of_charge TEXT
+    initial_storage_state_of_charge TEXT,
+    PRIMARY KEY(initial_state_type)
 );
 CREATE TABLE IF NOT EXISTS internal_gain_schedules (
     internal_gain_type TEXT,
     time_period TEXT,
     internal_gain_occupancy REAL DEFAULT 0,
     internal_gain_appliances REAL DEFAULT 0,
-    warm_water_demand REAL DEFAULT 0
+    warm_water_demand REAL DEFAULT 0,
+    PRIMARY KEY(internal_gain_type,time_period)
 );
 CREATE TABLE IF NOT EXISTS internal_gain_timeseries (
     internal_gain_type TEXT,
     time TEXT,
     internal_gain_occupancy REAL DEFAULT 0,
     internal_gain_appliances REAL DEFAULT 0,
-    warm_water_demand REAL DEFAULT 0
+    warm_water_demand REAL DEFAULT 0,
+    PRIMARY KEY(internal_gain_type,time)
 );
 CREATE TABLE IF NOT EXISTS internal_gain_types (
     internal_gain_type TEXT,
@@ -99,7 +110,8 @@ CREATE TABLE IF NOT EXISTS internal_gain_types (
     occupancy_co2_gain TEXT,
     occupancy_humidity_gain TEXT,
     appliances_heat_gain TEXT,
-    warm_water_demand_thermal_power TEXT
+    warm_water_demand_thermal_power TEXT,
+    PRIMARY KEY(internal_gain_type)
 );
 CREATE TABLE IF NOT EXISTS linearization_types (
     linearization_type TEXT,
@@ -113,22 +125,26 @@ CREATE TABLE IF NOT EXISTS linearization_types (
     linearization_zone_air_absolute_humidity TEXT,
     linearization_ambient_air_absolute_humidity TEXT,
     linearization_zone_air_co2_concentration TEXT,
-    linearization_zone_fresh_air_flow TEXT
+    linearization_zone_fresh_air_flow TEXT,
+    PRIMARY KEY(linearization_type)
 );
 CREATE TABLE IF NOT EXISTS parameters (
     parameter_set TEXT,
     parameter_name TEXT,
     parameter_value REAL,
     parameter_unit TEXT,
-    parameter_comment TEXT
+    parameter_comment TEXT,
+    PRIMARY KEY(parameter_set,parameter_name)
 );
 CREATE TABLE IF NOT EXISTS plant_cooling_types (
     plant_cooling_type TEXT,
-    plant_cooling_efficiency TEXT
+    plant_cooling_efficiency TEXT,
+    PRIMARY KEY(plant_cooling_type)
 );
 CREATE TABLE IF NOT EXISTS plant_heating_types (
     plant_heating_type TEXT,
-    plant_heating_efficiency TEXT
+    plant_heating_efficiency TEXT,
+    PRIMARY KEY(plant_heating_type)
 );
 CREATE TABLE IF NOT EXISTS scenarios (
     scenario_name TEXT,
@@ -139,7 +155,8 @@ CREATE TABLE IF NOT EXISTS scenarios (
     price_type TEXT,
     timestep_start TEXT,
     timestep_end TEXT,
-    timestep_interval TEXT
+    timestep_interval TEXT,
+    PRIMARY KEY(scenario_name)
 );
 CREATE TABLE IF NOT EXISTS storage_types (
     storage_type TEXT,
@@ -148,7 +165,8 @@ CREATE TABLE IF NOT EXISTS storage_types (
     storage_round_trip_efficiency TEXT,
     storage_battery_depth_of_discharge TEXT,
     storage_sensible_temperature_delta TEXT,
-    storage_self_discharge_rate TEXT
+    storage_self_discharge_rate TEXT,
+    PRIMARY KEY(storage_type)
 );
 CREATE TABLE IF NOT EXISTS surface_types (
     surface_type TEXT,
@@ -158,7 +176,8 @@ CREATE TABLE IF NOT EXISTS surface_types (
     emissivity_surface TEXT,
     window_type TEXT,
     window_wall_ratio TEXT,
-    sky_view_factor TEXT
+    sky_view_factor TEXT,
+    PRIMARY KEY(surface_type)
 );
 CREATE TABLE IF NOT EXISTS surfaces_adiabatic (
     building_name TEXT,
@@ -166,7 +185,9 @@ CREATE TABLE IF NOT EXISTS surfaces_adiabatic (
     surface_name TEXT,
     surface_type TEXT,
     surface_area TEXT,
-    surface_comment TEXT
+    surface_comment TEXT,
+    PRIMARY KEY(building_name,surface_name)
+    -- Note that the primary key constraint does not prevent duplicated `surface_name` in other surfaces tables.
 );
 CREATE TABLE IF NOT EXISTS surfaces_exterior (
     building_name TEXT,
@@ -175,7 +196,9 @@ CREATE TABLE IF NOT EXISTS surfaces_exterior (
     surface_name TEXT,
     surface_type TEXT,
     surface_area TEXT,
-    surface_comment TEXT
+    surface_comment TEXT,
+    PRIMARY KEY(building_name,surface_name)
+    -- Note that the primary key constraint does not prevent duplicated `surface_name` in other surfaces tables.
 );
 CREATE TABLE IF NOT EXISTS surfaces_interior (
     building_name TEXT,
@@ -184,13 +207,16 @@ CREATE TABLE IF NOT EXISTS surfaces_interior (
     surface_name TEXT,
     surface_type TEXT,
     surface_area TEXT,
-    surface_comment TEXT
+    surface_comment TEXT,
+    PRIMARY KEY(building_name,surface_name)
+    -- Note that the primary key constraint does not prevent duplicated `surface_name` in other surfaces tables.
 );
 CREATE TABLE IF NOT EXISTS window_types (
     window_type TEXT,
     heat_transfer_coefficient_conduction_window TEXT,
     absorptivity_window TEXT,
-    emissivity_window TEXT
+    emissivity_window TEXT,
+    PRIMARY KEY(window_type)
 );
 CREATE TABLE IF NOT EXISTS weather_timeseries (
     weather_type TEXT,
@@ -202,14 +228,16 @@ CREATE TABLE IF NOT EXISTS weather_timeseries (
     irradiation_east REAL,
     irradiation_south REAL,
     irradiation_west REAL,
-    irradiation_north REAL
+    irradiation_north REAL,
+    PRIMARY KEY(weather_type,time)
 );
 CREATE TABLE IF NOT EXISTS weather_types (
     weather_type TEXT,
     time_zone TEXT,
     latitude REAL,
     longitude REAL,
-    temperature_difference_sky_ambient REAL
+    temperature_difference_sky_ambient REAL,
+    PRIMARY KEY(weather_type)
 );
 CREATE TABLE IF NOT EXISTS zone_types (
     zone_type TEXT,
@@ -223,7 +251,8 @@ CREATE TABLE IF NOT EXISTS zone_types (
     hvac_vent_type TEXT,
     constraint_type TEXT,
     fresh_air_flow_control_type TEXT,
-    humidity_control_type TEXT
+    humidity_control_type TEXT,
+    PRIMARY KEY(zone_type)
 );
 CREATE TABLE IF NOT EXISTS zones (
     building_name TEXT,
@@ -231,5 +260,6 @@ CREATE TABLE IF NOT EXISTS zones (
     zone_type TEXT,
     zone_height TEXT,
     zone_area TEXT,
-    zone_comment TEXT
+    zone_comment TEXT,
+    PRIMARY KEY(building_name,zone_name)
 );
